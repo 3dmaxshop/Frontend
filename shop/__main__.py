2b750e20698cb41a32565deda9d952b96445245d
Dmitriy Volynkin <volynkin-da@yandex.ru>
2022-07-05 00:35:19 +0300

    feature/add-models-template

---
+++ shop/__main__.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+from shop.views import models
+
+app = Flask(__name__)
+
+app.register_blueprint(models.routes,url_prefix='/')
+
+@app.route('/')
+def index():
+    name_project = '3dMaxShop'
+    return render_template('index.html', content='3dshop',)
+
+
+if __name__ == "__main__":
+    app.run()
