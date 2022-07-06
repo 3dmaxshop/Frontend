e93d563c515285106e0190e79c01dbf9e80c9f2d
Dmitriy Volynkin <volynkin-da@yandex.ru>
2022-07-06 21:34:50 +0300

    feature/add-delete-model-button

--- shop/API/models.py
+++ shop/API/models.py
@@ -13,3 +13,7 @@ class ModelsApi:
         response.raise_for_status()
 
         return [Model(**model) for model in response.json()]
+
+    def delete(self, uid):
+        response = httpx.delete(f'{self.url}/api/v1/models/{uid}')
+        response.raise_for_status()
--- shop/views/models.py
+++ shop/views/models.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request, url_for
 
 from shop.api.client import client
 
@@ -9,3 +9,10 @@ routes = Blueprint('models', __name__)
 def get_all():
     models = client.models.get_all()
     return render_template('models.html', page_tittle='models_list', models=models)
+
+
+@routes.post('/delete')
+def delete():
+    uid = request.form['uid']
+    client.models.delete(uid)
+    return redirect(url_for('models.get_all'))
