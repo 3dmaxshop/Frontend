1027942af907ca333fe5c8fbb83f528ce2e68d2f
Dmitriy Volynkin <volynkin-da@yandex.ru>
2022-07-15 15:12:06 +0300

    feature/add-category-filter

--- shop/views/models.py
+++ shop/views/models.py
@@ -4,14 +4,24 @@ from flask import Blueprint, redirect, render_template, request, url_for
 
 from shop.api.client import client
 from shop.api.schemas import Model
+from shop.api.schemas import Category
 
 routes = Blueprint('models', __name__)
+
+
+def filter_category(models, category_id):
+
+    return [model for model in models if model.category_id == category_id]
 
 
 @routes.get('/')
 def get_all():
     models = client.models.get_all()
     categories = client.categories.get_all()
+    selected_category = request.args.get('category_id')
+    if selected_category:
+        models = filter_category(models, selected_category)
+
     return render_template('models.html', page_tittle='models_list', models=models, categories=categories)
 
 
@@ -44,3 +54,13 @@ def edit_page(uid):
     model = client.models.get_by_uid(uid)
     categories = client.categories.get_all()
     return render_template('model_edit.html', model=model, categories=categories)
+
+
+@routes.post('/search')
+def search_category():
+    payload = dict(request.form)
+
+    return redirect(url_for(
+        'models.get_all',
+        category_id=payload.get('category_id'),
+    ))
